6f1e79b89b35b5ea58cd1b1634c50b70aaf80787
emjorg10 <144714071+emjorg10@users.noreply.github.com>
2023-11-21 15:48:14 -0600

    signup now saves to database

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,36 +1,35 @@
-// import axios from "axios";
-// import { useState } from "react";
+import axios from "axios";
+import { useState } from "react";
 
 export function Signup() {
-  // const [errors, setErrors] = useState([]);
+  const [errors, setErrors] = useState([]);
 
-  // const handleSubmit = (event) => {
-  //   event.preventDefault();
-  //   setErrors([]);
-  //   const params = new FormData(event.target);
-  //   axios
-  //     .post("http://localhost:3000/users.json", params)
-  //     .then((response) => {
-  //       console.log(response.data);
-  //       event.target.reset();
-  //       window.location.href = "/"; // Change this to hide a modal, redirect to a specific page, etc.
-  //     })
-  //     .catch((error) => {
-  //       console.log(error.response.data.errors);
-  //       setErrors(error.response.data.errors);
-  //     });
-  // };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setErrors([]);
+    const params = new FormData(event.target);
+    axios
+      .post("http://localhost:3000/users.json", params)
+      .then((response) => {
+        console.log(response.data);
+        event.target.reset();
+        window.location.href = "/"; // Change this to hide a modal, redirect to a specific page, etc.
+      })
+      .catch((error) => {
+        console.log(error.response.data.errors);
+        setErrors(error.response.data.errors);
+      });
+  };
 
   return (
     <div id="signup">
       <h1>Signup</h1>
-      {/* <ul>
+      <ul>
         {errors.map((error) => (
           <li key={error}>{error}</li>
         ))}
-      </ul> */}
-      <form>
-        {/* <form onSubmit={handleSubmit}> */}
+      </ul>
+      <form onSubmit={handleSubmit}>
         <div>
           Name: <input name="name" type="text" />
         </div>
