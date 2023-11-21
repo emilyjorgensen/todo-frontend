613e8d02ba19722e91a28c3738aac1540bb5070c
emjorg10 <144714071+emjorg10@users.noreply.github.com>
2023-11-21 12:29:51 -0600

    login file added

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,9 +1,11 @@
 import { Signup } from "./Signup";
+import { Login } from "./Login";
 import { TodoNew } from "./TodoNew";
 
 export function Content() {
   return (
     <main>
+      <Login />
       <Signup />
       <h1>Make your to do list!</h1>
       <TodoNew />
